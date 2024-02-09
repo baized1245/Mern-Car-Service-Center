63e0288b32de73dea5ca8b2b87a93a8bc174d8da
unknown <you@example.com>
2024-02-09 19:17:13 +0600

    Set Basic Layout For Footer And Header

--- clint/src/Routes/Routes.jsx
+++ clint/src/Routes/Routes.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../Layout/Main'
+import Home from '../pages/Home/Home/Home'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
-    children: [{}],
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
   },
 ])
 
