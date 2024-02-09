d9d8bfcbf5286c28bf7a2303254396a036c1710c
unknown <you@example.com>
2024-02-09 22:59:56 +0600

    Load Services Fake Data And Display Service Card

---
+++ clint/src/pages/Home/Services/Services.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import ServiceCard from './ServiceCard'
+
+const Services = () => {
+  const [services, setServices] = useState([])
+
+  useEffect(() => {
+    fetch('services.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setServices(data)
+      })
+  }, [])
+
+  return (
+    <div className="mt-4">
+      <div className="text-center space-y-3">
+        <h3 className="text-2xl text-rose-500 font-bold">Service</h3>
+        <h3 className="text-5xl font-semibold">Our Services</h3>
+        <p className="text-black text-base">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem hic
+          asperiores consequuntur aliquam, <br /> aut cum excepturi, eos enim
+          quaerat sunt aperiam ipsum atque animi molestias!
+        </p>
+      </div>
+      {/* <div>
+        <h3 className="text-3xl">Service</h3>
+      </div> */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Services
