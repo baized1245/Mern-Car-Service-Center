90a4603bfd8006b72cb8415fdd65fe0718ba4fb0
unknown <you@example.com>
2024-02-10 04:39:03 +0600

    Create A Simple Login Page With Firebase Integration

--- clint/src/Routes/Routes.jsx
+++ clint/src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../Layout/Main'
 import Home from '../pages/Home/Home/Home'
+import Login from '../pages/Login/Login'
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
       },
     ],
   },
